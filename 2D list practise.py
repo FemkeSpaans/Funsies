415ce7177e7c9e8012b1fdeaaf13d4de3b047f17
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2019-11-07 18:29:17 +0100

    Add files via upload

---
+++ 2D list practise.py
@@ -0,0 +1,19 @@
+#Femke Spaans
+#05/11/2019
+#2D list practise
+
+file = open("Oefening_2D_lijst.xlsx","r") #open file
+D_list = [] #create empty list
+for line in file:
+    D_list.append(line)
+    print(D_list)
+
+##total = 0
+##for i in emptylist[3:]: 
+##        number = i[1]
+##        total+= int(number)
+    
+    
+        
+
+
